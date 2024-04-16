8c87dce1d02c5d433e668a61aabd89d2ba96aeee
Edu Caro <eduardo.caro.lorente@gmail.com>
2024-04-16 08:44:58 +0200

    pequeño avance

--- Alumno.java
+++ Alumno.java
@@ -1,14 +1,31 @@
 public class Alumno {
     private final int CEVA = 3;
     private final int CEJER = 5;
-    private int [][] matriz = new int [CEVA][CEJER];
+    private int [][] notas = new int [CEVA][CEJER];
     public void ponerNota(int nota, int eval, int ejer) throws EvaluacionIncorrecta,EjercicioIncorrecto{
-        if (eval>3 || eval<0){
-            throw new EvaluacionIncorrecta("La evaluación debe ser mayor que 0 y menor o igual a 3");
+        if(eval>3){
+            throw new EvaluacionIncorrecta("La evaluación debe ser como mucho 3");
         }
-        if(ejer<0 || ejer>5){
-            throw new EjercicioIncorrecto("El ejercicio debe ser mayor que 0 o menor o igual a 5");
+        if(eval<=0){
+            throw new EvaluacionIncorrecta("La evaluación no puede ser negativa ni cero");
         }
-        matriz[eval-1][ejer-1] = nota;
+        if(ejer>5){
+            throw new EjercicioIncorrecto("El ejercicio debe ser como mucho 5");
+        }
+        if(ejer<=0){
+            throw new EjercicioIncorrecto("El ejercicio no puede ser negativo ni cero");
+        }
+        notas[eval-1][ejer-1] = nota;
+    }
+    public int[][] getNotas() {
+        return notas;
+    }
+
+    public int getCEVA() {
+        return CEVA;
+    }
+
+    public int getCEJER() {
+        return CEJER;
     }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 public class Main {
+
     public static void main(String[] args) {
-        
+        Alumno a = new Alumno();
+        int nota = Leer.leerEntero("Introduce nota: ");
+        int evaluacion = Leer.leerEntero("Introduce evaluacion: ");
+        int ejercicio = Leer.leerEntero("Introduce ejercicio: ");
+        try{
+            a.ponerNota(nota,evaluacion,ejercicio);
+        }
+        catch(EvaluacionIncorrecta | EjercicioIncorrecto error){
+            System.out.println(error.getMessage());
+        }
+
     }
 }
