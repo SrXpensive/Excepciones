599587836a2af410129ec98a0523ba2e16da0917
Edu <eduardo.caro.lorente@gmail.com>
2024-04-17 20:25:15 +0200

    primera versión

--- Alumno.java
+++ Alumno.java
@@ -1,7 +1,14 @@
 public class Alumno {
     private final int CEVA = 3;
     private final int CEJER = 5;
-    private int [][] notas = new int [CEVA][CEJER];
+    private String nombre;
+    private String apellido;
+    private int [][] notas;
+    public Alumno(String nombre,String apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        notas = new int[CEVA][CEJER];
+    }
     public void ponerNota(int nota, int eval, int ejer) throws EvaluacionIncorrecta,EjercicioIncorrecto{
         if(eval>3){
             throw new EvaluacionIncorrecta("La evaluación debe ser como mucho 3");
@@ -27,5 +34,19 @@ public class Alumno {
 
     public int getCEJER() {
         return CEJER;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public void notas(){
+        for(int i = 0; i < CEVA; i++){
+            System.out.println("Notas de la "+(i+1)+" evaluación");
+           for(int j = 0; j < CEJER; j++){
+               System.out.println("Ejercicio "+(j+1)+": "+notas[i][j]);
+           }
+        }
     }
 }
--- Main.java
+++ Main.java
@@ -1,16 +1,57 @@
-public class Main {
+import java.util.ArrayList;
 
+public class Main {
+    static ArrayList <Alumno> alumnos = new ArrayList<>();
     public static void main(String[] args) {
-        Alumno a = new Alumno();
-        int nota = Leer.leerEntero("Introduce nota: ");
-        int evaluacion = Leer.leerEntero("Introduce evaluacion: ");
-        int ejercicio = Leer.leerEntero("Introduce ejercicio: ");
-        try{
-            a.ponerNota(nota,evaluacion,ejercicio);
-        }
-        catch(EvaluacionIncorrecta | EjercicioIncorrecto error){
-            System.out.println(error.getMessage());
-        }
+        int opcion;
+        String nombre;
+        String apellido;
+        int num;
+        int nota;
+        int evaluacion;
+        int ejercicio;
+        do{
+            System.out.println("1 - Añadir nuevo alumno");
+            System.out.println("2 - Poner una nota");
+            System.out.println("3 - Mostrar lista de notas de un alumno");
+            System.out.println("0 - Salir");
+            System.out.println("-------------------------");
+            opcion = Leer.leerEntero("Introduce una opción: ");
+            switch(opcion){
+                case 1:
+                    nombre = Leer.leerTexto("Introduce el nombre del alumno: ");
+                    apellido = Leer.leerTexto("Introduce el apellido del alumno: ");
+                    Alumno a = new Alumno(nombre,apellido);
+                    alumnos.add(a);
+                    break;
+                case 2:
+                    for(int i = 0; i < alumnos.size();i++){
+                        System.out.println((i+1)+" - "+alumnos.get(i).getNombre()+" "+alumnos.get(i).getApellido());
+                    }
+                    num = Leer.leerEntero("¿A qué alumno quieres ponerle la nota?: ");
+                    nota = Leer.leerEntero("Introduce nota: ");
+                    evaluacion = Leer.leerEntero("Introduce evaluacion: ");
+                    ejercicio = Leer.leerEntero("Introduce ejercicio: ");
+                    try{
+                        alumnos.get(num-1).ponerNota(nota,evaluacion,ejercicio);
+                    }
+                    catch(EvaluacionIncorrecta error1){
+                        System.out.println(error1.getMessage());
+                    }
+                    catch(EjercicioIncorrecto error2){
+                        System.out.println(error2.getMessage());
+                    }
+                    break;
+                case 3:
+                    for(int i = 0; i < alumnos.size();i++){
+                        System.out.println((i+1)+" - "+alumnos.get(i).getNombre()+" "+alumnos.get(i).getApellido());
+                    }
+                    num = Leer.leerEntero("¿De qué alumno quieres consultar las notas?: ");
+                    alumnos.get(num-1).notas();
+                    System.out.println("-------------------------");
+                    break;
+            }
+        }while(opcion !=0);
 
     }
 }
